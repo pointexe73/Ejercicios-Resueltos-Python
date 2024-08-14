2bb896f89db0e38f3bc23af3f255206ce3c4e6b4
pointexe73 <mejluis98@gmail.com>
2024-08-14 07:29:21 -0400

    Resuelto

---
+++ _6.py
@@ -0,0 +1,12 @@
+# Ejercicio 6: Obtener un Conjunto de Números Seperados por Coma y Crear una Lista
+
+
+ob_numeros = input("Numeros: ")
+
+print(ob_numeros)
+print(type(ob_numeros))
+
+numeros = ob_numeros.split(",")
+
+print(type(numeros))
+print(numeros)
