823eda3e29992c6c7a08490eb1fee501b427a8e2
pointexe73 <mejluis98@gmail.com>
2024-08-15 11:19:04 -0400

    Resuelto

---
+++ _7.py
@@ -0,0 +1,13 @@
+# Ejercicio 7: Obtener el Nombre de Extensión de un Nombre de Archivo
+
+# Escribe el nombre completo con la extencion del archivo
+nombre_archivo = input("Ingrese el nombre del archivo: ")
+
+# Ejemplo (main.py)
+# SPLIT() -> ['main', 'py'] Separa cada "." en una lista
+partes_nombre_archivo = nombre_archivo.split(".")
+
+print(partes_nombre_archivo)
+
+# Con -1 obtenemos el ultimo elemento de la lista
+print("La extensión del archivo es:", partes_nombre_archivo[-1])
