c67836a0dc80c98afb10ea037b1e2c37a9e226d4
pointexe73 <mejluis98@gmail.com>
2024-08-16 08:59:36 -0400

    Resuelto

--- _8.py
+++ _8.py
@@ -2,9 +2,12 @@
 
 # Crear una lista de numeros
 lista = [29, -5, -12, 17, 5, 24, 5, 12, 23, 16, 12, 5, -12, 17]
+
 # lista[0] llama al primer elementos de la lista [29]
 lista1 = lista[0]
+
 #lista[-1] llama al ultimo elemento de la lista [17]
 lista2 = lista[-1]
+
 # Imprimer el primer y ultimo elemento de la lista
 print(f'El primer elemento es: {lista1} y el segundo elemento es: {lista2}')
