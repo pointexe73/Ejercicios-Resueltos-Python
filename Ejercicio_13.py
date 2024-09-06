0bc93a98ee983ccb8721f9271943d11ceeb55271
pointexe73 <mejluis98@gmail.com>
2024-09-06 07:38:51 -0400

    Clase Completada

--- Ejercicio_13.py
+++ Ejercicio_13.py
@@ -1,2 +1,10 @@
 # Ejercicio 13: Crear una Cadena de Caracteres Multilínea
 
+cadena = """
+            Python es un lenguaje de 
+            programacion indispensable
+            para cualquier informatico
+            o aprendiz del desarrollo.
+        """
+
+print(cadena)
