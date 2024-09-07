64090427d629c89d7747b54e451376a2da91e498
pointexe73 <mejluis98@gmail.com>
2024-09-07 00:37:59 -0400

    Clase Completada

--- Ejercicio_019.py
+++ Ejercicio_019.py
@@ -8,4 +8,10 @@ def validar_nombre_archivo(nombre_archivo):
     Validar si un nombre de archivo tiene la extencion .py
     Si el archivo no tiene la extencion, la carga.
     """
-    if len (nombre_archivo) >= 3 and nombre_archivo
+    if len (nombre_archivo) >= 3 and nombre_archivo[-3:] == '.py':
+        return nombre_archivo
+    
+    return nombre_archivo + '.py' 
+
+print(validar_nombre_archivo('main.py'))
+print(validar_nombre_archivo('modulo'))
